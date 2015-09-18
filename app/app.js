3f5460e708952965abdef4c2a4548fe7123a4393
Gourav Anvekar <gourav.anvekar@gmail.com>
2015-09-18 13:56:41 -0400

    Added position filters using links

--- app/app.js
+++ app/app.js
@@ -2,10 +2,14 @@
 	var app = angular.module('myVote', []);
 
     app.controller('VoteController', ['$scope', '$http', function($scope, $http){
-
         $http.get('voting_data.json').then(function(voteData){
             $scope.candidates = voteData.data;
             console.log(candidates[0]);
         });
+        //$scope.positionFilter = "President";
+
+        $scope.setPosition = function(position){
+            $scope.positionFilter = position;
+        }
     }]);
 })();
